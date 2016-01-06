05ed315e4b4ee1509e84da2f5f84ab5c0c1cb186
david.lv <david.ullua@gmail.com>
2016-01-06 23:01:30 +0800

    aliyun whitelist service

---
+++ settings.py
@@ -0,0 +1,6 @@
+appKey = 'key'
+appSecret = 'secret'
+zone = 'cn-hangzhou'
+
+white_list = '127.0.0.1'  #BJ office
+white_ip_prefix = ''
---
+++ whitelist.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_url_path='')
+
+from settings import appKey,appSecret,zone,white_list,white_ip_prefix
+
+@app.route('/')
+def root():
+	return "aliyun whitelist service";
+
+
+def allow(ip):
+	return ip in white_list or (white_ip_prefix and ip.startswith(white_ip_prefix))
+
+@app.route('/list')
+def list():
+	#print(request.remote_addr)
+	if not allow(request.remote_addr):
+		return "-1"
+	return white_list + ' ' +  aliyun_ecs_list()
+
+def aliyun_ecs_list():
+	from aliyunsdkcore import client
+	from aliyunsdkcore import request
+	from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
+	clt = client.AcsClient(appKey,appSecret, zone)
+	request = DescribeInstancesRequest.DescribeInstancesRequest()
+	result = clt.do_action(request)
+
+	from pyquery import PyQuery as pq
+	d = pq(result,parser='xml')
+	ip_list = d('PublicIpAddress').text()
+	internal_ip_list = d('InnerIpAddress').text()
+	return ip_list + ' ' + internal_ip_list
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run(host="0.0.0.0",port=5001)
